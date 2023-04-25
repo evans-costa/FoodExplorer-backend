e29a379735af138600fac30941d26c1ae96ed6e3
Evandro <evandro.souzac@gmail.com>
2023-04-25 10:22:53 -0300

    Creating error handling utils

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
+import "express-async-errors";
 import express from "express";
+import { AppError } from "./utils/AppError.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
