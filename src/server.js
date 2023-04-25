8d0284bfbb970f4b4d70136e7f26402f6f668599
Evandro <evandro.souzac@gmail.com>
2023-04-25 14:03:21 -0300

    install dotenv and jwt

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
+import * as dotenv from "dotenv";
 import "express-async-errors";
 import express from "express";
 import database from "./database/sqlite/index.js";
 import { routes } from "./routes/index.js";
 import { AppError } from "./utils/AppError.js";
 
-const app = express();
+dotenv.config();
 database();
+
+const app = express();
 
 app.use(express.json());
 app.use(routes);
@@ -23,11 +26,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
